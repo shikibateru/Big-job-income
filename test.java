9cb61169e8ee163fa046908846ed2f1e4edd255c
Sviatoslav <sviatoslavon@gmail.com>
2022-10-09 21:24:49 +0300

    rdy

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.print("Hello Friend!");
+        System.out.println("Hello Friend!");
+        System.out.print("Artem lodd");
     }
 }
