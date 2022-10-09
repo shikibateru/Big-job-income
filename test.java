fd34a8db432f4c2617a88d619bf21e03d7258c5e
Artem <feverpayne@gmail.com>
2022-10-09 21:29:56 +0300

    final

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello Friend!");
         System.out.print("Artem lodd");
+        System.out.println("Hello Sviatyik");
     }
 }
