6c716c8a1b57130fdcbebc2a2eb67561f11d22c3
Artem <feverpayne@gmail.com>
2022-10-09 23:28:34 +0300

    plan1

---
+++ DTO/SignupRequestDTO.java
@@ -0,0 +1,6 @@
+package DTO;
+/*Data Transfer Object потрібні, щоб перекидати зменшені по полях
+сутності всередині сервера. Наприклад, оперувати ними у запитах API*/
+
+public class SignupRequestDTO {
+}
--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello Friend!");
-        System.out.print("Artem lodd");
-        System.out.println("Hello Sviatyik");
-        System.out.println("Don`t matter");
-    }
-}
